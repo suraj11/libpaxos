3817e19fc3bf232f48f59e10e56370d7a2b99d8d
Ubuntu <ubuntu@ip-172-31-26-28.us-west-2.compute.internal>
2017-06-06 04:27:56 +0000

    implement async client

--- src/paxos_initiator.cc
+++ src/paxos_initiator.cc
@@ -12,7 +12,6 @@
 #include <grpc++/create_channel.h>
 #include <grpc++/security/credentials.h>
 #include "paxos.grpc.pb.h"
-#include <glog/logging.h>
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -20,6 +19,8 @@ using grpc::ClientReader;
 using grpc::ClientReaderWriter;
 using grpc::ClientWriter;
 using grpc::Status;
+using grpc::CompletionQueue;
+
 using libpaxos::NextRound;
 using libpaxos::LastVote;
 using libpaxos::BeginRound;
@@ -27,7 +28,6 @@ using libpaxos::Voted;
 using libpaxos::Ok;
 using libpaxos::Value;
 using libpaxos::Acceptor;
-using namespace std;
 
 class InitiatorClient {
 private:
@@ -63,60 +63,73 @@ public:
 int main(int argc, char** argv) {
 
   InitiatorClient client;
-  google::InitGoogleLogging(argv[0]);
 
   std::ifstream file("../src/acceptor.conf");
   std::string str;
-  vector<string> address;
-  while (std::getline(file, str))
-  {
+  std::vector<std::string> address;
+  while (std::getline(file, str)) {
     address.push_back(str);
   }
 
-  vector<std::shared_ptr<Channel>> channel(address.size());
-  vector<LastVote> response(address.size());
+  std::vector<std::shared_ptr<Channel>> channel(address.size());
+  std::vector<LastVote> response(address.size());
   uint64_t maxRound = 0;
   uint64_t maxVal = 1;
   uint64_t thisRoundNum = client.getNextRoundNumber();
   client.setLastTried();
 
+  for (int i=0; i < address.size(); i++) {
+    channel[i] = grpc::CreateChannel(address[i], grpc::InsecureChannelCredentials());
+  }
+
 /* Next ballot */
   for(int i=0; i < address.size(); i++) {
-      channel[i] = grpc::CreateChannel(address[i], grpc::InsecureChannelCredentials());
-      std::unique_ptr<Acceptor::Stub> stub_(Acceptor::NewStub(channel[i]));
-      ClientContext contextGetlastVote;
-      NextRound round;
-      cout << "created channel for "<<address[i]<< "\n";
-      cout<<"before getting last vote.\n";
-      round.set_roundnumber(thisRoundNum);
-      Status status = stub_->getLastVote(&contextGetlastVote, round, &response[i]);
-      cout<<"got last vote.\n";
+    auto stub_ = Acceptor::NewStub(channel[i]);
+    ClientContext contextGetlastVote;
+    NextRound round;
+    CompletionQueue cq;
+    std::cout << "created channel for " << address[i] << std::endl;
+    std::cout << "before getting last vote." << std::endl;
+    round.set_roundnumber(thisRoundNum);
+    auto rpc = stub_->AsyncgetLastVote(&contextGetlastVote, round, &cq);
+    std::cout << "got last vote." << std::endl;
 
+    Status status;
+    rpc->Finish(&response[i], &status, (void*)&i);
+
+    void* got_tag;
+    bool ok = false;
+    std::cout << "before cq" << std::endl;
+    cq.Next(&got_tag, &ok);
+    if (ok && got_tag == (void*)&i) {
+      std::cout << response[i].lastvalue() << std::endl;
       if(response[i].accepted() && response[i].lastround() > maxRound) {
         maxRound = response[i].lastround();
         maxVal = response[i].lastvalue();
       }
+    } else {
+      std::cout << "INVAID!!!" << std::endl;
+    }
   }
 
 
 /* BeginBallot */
   for(int i=0; i < address.size(); i++) {
-    channel[i] = grpc::CreateChannel(address[i], grpc::InsecureChannelCredentials());
-    std::unique_ptr<Acceptor::Stub> stub_(Acceptor::NewStub(channel[i]));
+    auto stub_ = Acceptor::NewStub(channel[i]);
 
     ClientContext contextBeginRound;
     BeginRound br;
     br.set_roundnumber(thisRoundNum);
     br.set_value(maxVal);
     Voted vote;
-    cout<<"before begin round\n";
+    std::cout << "before begin round" << std::endl;
     stub_->beginRound(&contextBeginRound, br, &vote);
 
     if(vote.accepted() && vote.roundnumber() == thisRoundNum) {
       ClientContext contextSuccess;
       Value v;
       Ok k;
-      cout<<"before success.\n";
+      std::cout << "before success" << std::endl;
       client.setVal(maxVal);
       stub_->success(&contextSuccess, v, &k);
     }
