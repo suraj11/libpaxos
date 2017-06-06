a218dd2f812504ad6beb20ecc09788a7d4127a07
Ubuntu <ubuntu@ip-172-31-26-28.us-west-2.compute.internal>
2017-06-06 02:29:22 +0000

    odified initiator

--- src/paxos_initiator.cc
+++ src/paxos_initiator.cc
@@ -12,6 +12,7 @@
 #include <grpc++/create_channel.h>
 #include <grpc++/security/credentials.h>
 #include "paxos.grpc.pb.h"
+#include <glog/logging.h>
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -29,14 +30,42 @@ using libpaxos::Acceptor;
 using namespace std;
 
 class InitiatorClient {
+private:
+  uint64_t value;
+  uint64_t lastTried;
+
 public:
+  InitiatorClient() {
+    lastTried = 0;
+  }
+
+  void setVal(uint64_t val) {
+    value = val;
+  }
+
+  uint64_t getVal() {
+    return value;
+  }
+
+  void setLastTried() {
+     lastTried++;
+  }
+
+  uint64_t getLastTried() {
+    return lastTried;
+  }
+
+  uint64_t getNextRoundNumber() {
+    return lastTried + 1;
+  }
 };
 
 int main(int argc, char** argv) {
 
   InitiatorClient client;
+  google::InitGoogleLogging(argv[0]);
 
-  std::ifstream file("acceptor.conf");
+  std::ifstream file("../src/acceptor.conf");
   std::string str;
   vector<string> address;
   while (std::getline(file, str))
@@ -45,10 +74,11 @@ int main(int argc, char** argv) {
   }
 
   vector<std::shared_ptr<Channel>> channel(address.size());
-  vector<LastVote> response;
-  int maxRound = INT_MIN;
-  int maxVal = INT_MIN;
-  uint64_t lastTried = 1;
+  vector<LastVote> response(address.size());
+  uint64_t maxRound = 0;
+  uint64_t maxVal = 1;
+  uint64_t thisRoundNum = client.getNextRoundNumber();
+  client.setLastTried();
 
 /* Next ballot */
   for(int i=0; i < address.size(); i++) {
@@ -56,18 +86,16 @@ int main(int argc, char** argv) {
       std::unique_ptr<Acceptor::Stub> stub_(Acceptor::NewStub(channel[i]));
       ClientContext contextGetlastVote;
       NextRound round;
-      cout<< "created channel for "<<address[i]<< "\n";
-      round.set_roundnumber(lastTried);
+      cout << "created channel for "<<address[i]<< "\n";
       cout<<"before getting last vote.\n";
+      round.set_roundnumber(thisRoundNum);
       Status status = stub_->getLastVote(&contextGetlastVote, round, &response[i]);
       cout<<"got last vote.\n";
 
-/*      if(response[i].lastround() > maxRound) {
+      if(response[i].accepted() && response[i].lastround() > maxRound) {
         maxRound = response[i].lastround();
         maxVal = response[i].lastvalue();
-        maxVal = 1;
       }
-*/
   }
 
 
@@ -78,19 +106,20 @@ int main(int argc, char** argv) {
 
     ClientContext contextBeginRound;
     BeginRound br;
-    br.set_roundnumber(lastTried);
+    br.set_roundnumber(thisRoundNum);
     br.set_value(maxVal);
     Voted vote;
- cout<<"before begin round\n";
+    cout<<"before begin round\n";
     stub_->beginRound(&contextBeginRound, br, &vote);
 
-//    if(vote.lastround() == lastTried) {
+    if(vote.accepted() && vote.roundnumber() == thisRoundNum) {
       ClientContext contextSuccess;
       Value v;
       Ok k;
       cout<<"before success.\n";
+      client.setVal(maxVal);
       stub_->success(&contextSuccess, v, &k);
-//    }
+    }
    }
 
   return 0;
