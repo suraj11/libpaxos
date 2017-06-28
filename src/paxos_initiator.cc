20c2e7bd644cbc8f3306b72b9f0cff636ac080c8
Ubuntu <ubuntu@ip-172-31-26-28.us-west-2.compute.internal>
2017-06-28 02:24:21 +0000

    remove main from initiator

--- src/paxos_initiator.cc
+++ src/paxos_initiator.cc
@@ -58,83 +58,79 @@ public:
   uint64_t getNextRoundNumber() {
     return lastTried + 1;
   }
-};
 
-int main(int argc, char** argv) {
+  int initiateRound() {
 
-  InitiatorClient client;
+    std::ifstream file("../src/acceptor.conf");
+    std::string str;
+    std::vector<std::string> address;
+    while (std::getline(file, str)) {
+      address.push_back(str);
+    }
 
-  std::ifstream file("../src/acceptor.conf");
-  std::string str;
-  std::vector<std::string> address;
-  while (std::getline(file, str)) {
-    address.push_back(str);
-  }
+    std::vector<std::shared_ptr<Channel>> channel(address.size());
+    std::vector<LastVote> response(address.size());
+    uint64_t maxRound = 0;
+    uint64_t maxVal = 1;
+    uint64_t thisRoundNum = getNextRoundNumber();
+    setLastTried();
 
-  std::vector<std::shared_ptr<Channel>> channel(address.size());
-  std::vector<LastVote> response(address.size());
-  uint64_t maxRound = 0;
-  uint64_t maxVal = 1;
-  uint64_t thisRoundNum = client.getNextRoundNumber();
-  client.setLastTried();
+    for (int i=0; i < address.size(); i++) {
+      channel[i] = grpc::CreateChannel(address[i], grpc::InsecureChannelCredentials());
+    }
 
-  for (int i=0; i < address.size(); i++) {
-    channel[i] = grpc::CreateChannel(address[i], grpc::InsecureChannelCredentials());
-  }
+  /* Next ballot */
+    for(int i=0; i < address.size(); i++) {
+      auto stub_ = Acceptor::NewStub(channel[i]);
+      ClientContext contextGetlastVote;
+      NextRound round;
+      CompletionQueue cq;
+      std::cout << "created channel for " << address[i] << std::endl;
+      std::cout << "before getting last vote." << std::endl;
+      round.set_roundnumber(thisRoundNum);
+      auto rpc = stub_->AsyncgetLastVote(&contextGetlastVote, round, &cq);
+      std::cout << "got last vote." << std::endl;
 
-/* Next ballot */
-  for(int i=0; i < address.size(); i++) {
-    auto stub_ = Acceptor::NewStub(channel[i]);
-    ClientContext contextGetlastVote;
-    NextRound round;
-    CompletionQueue cq;
-    std::cout << "created channel for " << address[i] << std::endl;
-    std::cout << "before getting last vote." << std::endl;
-    round.set_roundnumber(thisRoundNum);
-    auto rpc = stub_->AsyncgetLastVote(&contextGetlastVote, round, &cq);
-    std::cout << "got last vote." << std::endl;
+      Status status;
+      rpc->Finish(&response[i], &status, (void*)&i);
 
-    Status status;
-    rpc->Finish(&response[i], &status, (void*)&i);
-
-    void* got_tag;
-    bool ok = false;
-    std::cout << "before cq" << std::endl;
-    cq.Next(&got_tag, &ok);
-    if (ok && got_tag == (void*)&i) {
-      std::cout << response[i].lastvalue() << std::endl;
-      if(response[i].accepted() && response[i].lastround() > maxRound) {
-        maxRound = response[i].lastround();
-        maxVal = response[i].lastvalue();
+      void* got_tag;
+      bool ok = false;
+      std::cout << "before cq" << std::endl;
+      cq.Next(&got_tag, &ok);
+      if (ok && got_tag == (void*)&i) {
+        std::cout << response[i].lastvalue() << std::endl;
+        if(response[i].accepted() && response[i].lastround() > maxRound) {
+          maxRound = response[i].lastround();
+          maxVal = response[i].lastvalue();
+        }
+      } else {
+        std::cout << "INVAID!!!" << std::endl;
       }
-    } else {
-      std::cout << "INVAID!!!" << std::endl;
     }
-  }
 
 
-/* BeginBallot */
-  for(int i=0; i < address.size(); i++) {
-    auto stub_ = Acceptor::NewStub(channel[i]);
+  /* BeginBallot */
+    for(int i=0; i < address.size(); i++) {
+      auto stub_ = Acceptor::NewStub(channel[i]);
 
-    ClientContext contextBeginRound;
-    BeginRound br;
-    br.set_roundnumber(thisRoundNum);
-    br.set_value(maxVal);
-    Voted vote;
-    std::cout << "before begin round" << std::endl;
-    stub_->beginRound(&contextBeginRound, br, &vote);
+      ClientContext contextBeginRound;
+      BeginRound br;
+      br.set_roundnumber(thisRoundNum);
+      br.set_value(maxVal);
+      Voted vote;
+      std::cout << "before begin round" << std::endl;
+      stub_->beginRound(&contextBeginRound, br, &vote);
 
-    if(vote.accepted() && vote.roundnumber() == thisRoundNum) {
-      ClientContext contextSuccess;
-      Value v;
-      Ok k;
-      std::cout << "before success" << std::endl;
-      client.setVal(maxVal);
-      stub_->success(&contextSuccess, v, &k);
-    }
+      if(vote.accepted() && vote.roundnumber() == thisRoundNum) {
+        ClientContext contextSuccess;
+        Value v;
+        Ok k;
+        std::cout << "before success" << std::endl;
+        setVal(maxVal);
+        stub_->success(&contextSuccess, v, &k);
+      }
+     }
    }
-
-  return 0;
-}
+};
 
